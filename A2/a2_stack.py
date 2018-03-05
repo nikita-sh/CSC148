51edc0be7cbbfd9e7e13cbdaf98136f522521716
Nikita Shumeiko <36054510+nikita-sh@users.noreply.github.com>
2018-03-05 16:24:45 -0500

    Create a2_stack.py

---
+++ A2/a2_stack.py
@@ -0,0 +1,67 @@
+"""
+Stack ADT for A2 Iterative minimax.
+"""
+
+from typing import List
+
+
+class Stack:
+    """
+    Last-in, first-out (LIFO) stack.
+
+    ===Attributes===
+    _contents: contents of the stack
+    """
+    _contents: List[object]
+
+    def __init__(self) -> None:
+        """
+        Create a new, empty Stack self.
+
+        >>> s = Stack()
+        """
+        self._contents = []
+
+    def add(self, obj: object) -> None:
+        """
+        Add object obj to top of Stack self.
+
+        >>> s = Stack()
+        >>> s.add(7)
+        """
+        self._contents.append(obj)
+
+    def remove(self) -> object:
+        """
+        Remove and return top element of Stack self.
+
+        Assume Stack self is not empty.
+
+        >>> s = Stack()
+        >>> s.add(5)
+        >>> s.add(7)
+        >>> s.remove()
+        7
+        """
+        if self.is_empty():
+            raise IndexError("Stack is empty.")
+        else:
+            return self._contents.pop()
+
+    def is_empty(self) -> bool:
+        """
+        Return whether Stack self is empty.
+
+        >>> s = Stack()
+        >>> s.is_empty()
+        True
+        >>> s.add(7)
+        >>> s.is_empty()
+        False
+        """
+        return len(self._contents) == 0
+
+
+if __name__ == "__main__":
+    from python_ta import check_all
+    check_all(config="a2_pyta.txt")
